4823aabcfe6891a87f896b1d0f0174e432395501
EC2 Default User <ec2-user@ip-172-31-38-226.ec2.internal>
2020-12-09 18:16:27 +0000

    message

--- javascript/jsSandbox.js
+++ javascript/jsSandbox.js
@@ -1,41 +1,18 @@
-/*
-console.log('hello world');
-
-document.write('hello world<br>');
-
-
-var myVar = 'today'
-
-document.write(`Today is ${myVar}`);
-
-var beginEffect = '<strong>';
-var endEffect = '</string>';
-var bPara = '<p>';
-var ePara = '</p>';
-var headingText = '<h1>A Page of Javascript</h1>';
-var myIntro = 'Hello, welcome to my Javascript page!';
-var linkTag = '<a href="http://www.scripttheweb.com">Link to a site</a>';
-var redText = '<span style="color:red">I am so colorful today!</span>';
-
-document.write(headingText);
-document.write(beginEffect + myIntro + endEffect);
-document.write(`${bPara}${linkTag}${ePara}`);
-document.write(bPara + redText + ePara);
-
-*/
 
 console.log('wtf');
 
 var xhr = new XMLHttpRequest();
-xhr.onreadystatechange= function() {
+xhr.onreadystatechange = function() {
     if(xhr.readyState === 4) {
+        console.log('line 32');
         if((xhr.status >= 200 && xhr.status < 300) || (xhr.status === 304)){
             console.log(xhr.status);
             console.log(xhr.responseText);
             alert(xhr.responseText);
-            document.getElementById('demo').innerHTML = xhr(.responseText);
+            document.getElementById('demo').innerHTML = xhr.responseText;
         }
     }
 }
+console.log('line 41');
 xhr.open('get', 'descriptiveWords.txt', true);
 xhr.send(null);

0461682bd78561291aad4e8f7a9aea84a882e52a
EC2 Default User <ec2-user@ip-172-31-38-226.ec2.internal>
2020-12-09 19:45:37 +0000

    message

--- javascript/jsSandbox.js
+++ javascript/jsSandbox.js
@@ -25,7 +25,7 @@ function loadDoc(url, cFunction) {
         if(xhr.readyState === 4) {
             console.log('line 4');
             if((xhr.status >= 200 && xhr.status < 300) || (xhr.status === 304)){
-                cFunction(this); 
+                return cFunction(this); 
             }
         }
     };

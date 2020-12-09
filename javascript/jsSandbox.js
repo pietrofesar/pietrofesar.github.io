20e3c8065b89c5c80672a45c41026b9376ed4d5d
EC2 Default User <ec2-user@ip-172-31-38-226.ec2.internal>
2020-12-09 17:51:27 +0000

    message

--- javascript/jsSandbox.js
+++ javascript/jsSandbox.js
@@ -30,6 +30,8 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange= function() {
     if(xhr.readyState === 4) {
         if((xhr.status >= 200 && xhr.status < 300) || (xhr.status === 304)){
+            console.log(xhr.status);
+            console.log(xhr.responseText);
             alert(xhr.responseText);
         }
     }

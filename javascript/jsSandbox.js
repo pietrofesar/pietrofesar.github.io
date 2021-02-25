be9834c3afeadb0100a377526506631271f2c0cc
EC2 Default User <ec2-user@ip-172-31-38-226.ec2.internal>
2021-02-25 13:53:23 +0000

    message

--- javascript/jsSandbox.js
+++ javascript/jsSandbox.js
@@ -1,45 +1,5 @@
-/*
-// this example is for a single doc
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-        console.log('line 4');
-        if ((xhr.status >= 200 && xhr.status < 300) || (xhr.status === 304)) {
-            console.log(xhr.status);
-            console.log(xhr.responseText);
-            alert(xhr.responseText);
-            document.getElementById('demo').innerHTML = xhr.responseText;
-        }
-    }
+document.addEventListener('keydown', logKey);
+
+function logKey(){
+    document.querySelector('#demo').innerHTML='a key was pressed';
 }
-console.log('line 13');
-xhr.open('get', 'descriptiveWords.txt', true);
-xhr.send(null);
-*/
-
-
-// this example is for multiple docs
-function loadDoc(url, cFunction) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4) {
-            console.log('line 4');
-            if((xhr.status >= 200 && xhr.status < 300) || (xhr.status === 304)){
-                cFunction(this); 
-            }
-        }
-    };
-    xhr.open('get', url, true);
-    xhr.send();
-}
-
-// function for a doc, define another for another doc
-function myFunction(xhr) {
-    var descriptiveWords = xhr.responseText.split(' ');
-    document.getElementById('demo').innerHTML = xhr.responseText;
-}
-
-loadDoc('descriptiveWords.txt', myFunction);
-
-
-
